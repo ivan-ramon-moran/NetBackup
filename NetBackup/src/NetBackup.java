a6400adee59c357e9e5dd2bc4e6508872a09103d
K3rneL <K3rneL@K3rneL-PC>
2013-09-25 23:33:14 +0200

    aaaa

--- NetBackup/src/NetBackup.java
+++ NetBackup/src/NetBackup.java
@@ -54,6 +54,7 @@ public class NetBackup extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+    	//HOLA HOLA HOLA
     	//----------------------INICIAMOS EL CLIENTE------------------------------
     	IniciarCliente();
     	//------------------------------------------------------------------------
