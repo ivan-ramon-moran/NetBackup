a8be0f2a0f5da38c90dc96298fb44fa3e4386ff7
Adri <Adri@Adri-PC.mshome.net>
2013-10-03 00:43:13 +0200

    Identifica automáticamente la carpeta que tiene que monitorizar (Escritorio)

--- NetBackup/src/SistemaOperativo.java
+++ NetBackup/src/SistemaOperativo.java
@@ -1,5 +1,5 @@
-
 public class SistemaOperativo {
+	
 	public static String getSistema(){
 		return System.getProperty("os.name" );
 	}
@@ -12,5 +12,15 @@ public class SistemaOperativo {
 		return (getSistema().contains("Lin") || getSistema().contains("lin"));
 	}
 	
-	
+	public static String getPath(String nombre){
+		String ruta = "";
+		if (isWindows()){
+			ruta = ("C:\\Users\\" + nombre + "\\Desktop");
+		}
+		if (isLinux()){
+			ruta = ("/home/" + nombre + "/Escritorio");
+		}
+		
+		return ruta;
+	}
 }
