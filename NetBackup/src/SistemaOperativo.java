f2f25c6c66977e2478391ad2c28789a158df9d53
K3rneL <K3rneL@K3rneL-PC>
2013-10-07 22:10:24 +0200

    a

--- NetBackup/src/SistemaOperativo.java
+++ NetBackup/src/SistemaOperativo.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.util.ArrayList;
+
 
 public class SistemaOperativo {
+	
+	public static ArrayList<String> ficheros = new ArrayList<String>();
+	
 	public static String getSistema(){
 		return System.getProperty("os.name" );
 	}
@@ -12,5 +18,23 @@ public class SistemaOperativo {
 		return (getSistema().contains("Lin") || getSistema().contains("lin"));
 	}
 	
+	
+	public static void buscarDatosSensibles(String ruta){
+		File file = new File(ruta);
+		String [] elementos = file.list();
+		
+		if (elementos != null){
+			for (int i = 0; i < elementos.length; i++){
+				File file2 = new File(ruta + "/" + elementos[i]);
+				
+				if (file2.isDirectory()){
+					buscarDatosSensibles(ruta + "/" + file2.getName());
+				}
+				else{
+					ficheros.add(file2.getName());
+				}
+			}
+		}
+	}
 	
 }
